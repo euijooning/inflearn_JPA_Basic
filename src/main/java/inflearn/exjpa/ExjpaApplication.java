429ee65b9ad5799225424161ab514837d4a8affb
EUIJOON <euijoonism@gmail.com>
2023-08-10 20:17:57 +0900

    First Commit

---
+++ src/main/java/inflearn/exjpa/ExjpaApplication.java
@@ -0,0 +1,13 @@
+package inflearn.exjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExjpaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExjpaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/inflearn/exjpa/ExjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn.exjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExjpaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
