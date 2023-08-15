f5d9eea32252015a48182c60234f13c72393c4e4
EUIJOON <euijoonism@gmail.com>
2023-08-15 19:32:03 +0900

    섹션 4. 엔티티 매핑 - 기본 키 매핑 SEQUENCE 전략 부연설명 예시

--- src/main/java/inflearn/exjpa/JpaMain.java
+++ src/main/java/inflearn/exjpa/JpaMain.java
@@ -15,13 +15,33 @@ public class JpaMain {
     tx.begin();
 
     try {
-      Member member = new Member();
-      member.setUsername("C");
+//      Member member = new Member();
+//      member.setUsername("D");
+//
+//      System.out.println("=======================");
+//      em.persist(member);
+//      System.out.println("member.id = " + member.getId());
+//      System.out.println("=======================");
+      Member member1 = new Member();
+      member1.setUsername("A");
 
-      System.out.println("======================");
-      em.persist(member);
-      System.out.println("member.id = " +member.getId());
-      System.out.println("======================");
+      Member member2 = new Member();
+      member2.setUsername("B");
+
+      Member member3 = new Member();
+      member3.setUsername("C");
+
+      System.out.println("==============");
+
+      em.persist(member1); // 1, 51
+      em.persist(member2); // 메모리에서
+      em.persist(member3); // 메모리에서
+
+      System.out.println("member1 = " + member1.getId());
+      System.out.println("member2 = " + member2.getId());
+      System.out.println("member3 = " + member3.getId());
+
+      System.out.println("==============");
 
       tx.commit();
     } catch (Exception e) {
