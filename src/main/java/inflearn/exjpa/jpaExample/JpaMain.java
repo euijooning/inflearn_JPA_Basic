a540fdff3e0396050d9f3ff17a866de2adf56b07
EUIJOON <euijoonism@gmail.com>
2023-08-17 13:23:33 +0900

    섹션 5. 연관관계 매핑 기초 - 단방향 연관관계(2). 객체 지향 모델링. 객체 연관관계 사용

--- src/main/java/inflearn/exjpa/jpaExample/JpaMain.java
+++ src/main/java/inflearn/exjpa/jpaExample/JpaMain.java
@@ -23,15 +23,30 @@ public class JpaMain {
       //회원 저장
       Member member = new Member();
       member.setNane("member1");
-      member.setTeamId(team.getId()); // member.setTeam(); 이 아니다.
+//      member.setTeamId(team.getId()); // member.setTeam(); 이 아니다.
+      member.setTeam(team);
+      // 이러면 Jpa가 팀에서 알아서 pk값을 꺼내서 Insert할 때 FK 값으로 사용한다.
+
       em.persist(member);
 
+      em.flush();
+      em.clear();
 
+//      Member findMember = em.find(Member.class, member.getId());
+//
+//      Long findTeamId = findMember.getTeamId();;;
+//      Team findTeam = em.find(Team.class, findTeamId);
+
+      // 조회할 때도 단순해짐
       Member findMember = em.find(Member.class, member.getId());
+      // 1차캐시에서 가져오는 것 잊어버리지 말기
 
-      Long findTeamId = findMember.getTeamId();;;
-      Team findTeam = em.find(Team.class, findTeamId);
+      Team findTeam = findMember.getTeam();
+      System.out.println("팀찾기 = " + findTeam.getName());
 
+//      // 수정 예시
+//      Team newTeam = em.find(Team.class, 100L); // 아이디 100번인 팀이 있다고 가졍
+//      findMember.setTeam(newTeam);
       tx.commit();
     } catch (Exception e) {
       tx.rollback();
--- src/main/java/inflearn/exjpa/jpaExample/Member.java
+++ src/main/java/inflearn/exjpa/jpaExample/Member.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.TableGenerator;
 import lombok.NoArgsConstructor;
@@ -21,9 +23,15 @@ public class Member {
   @Column(name = "USERNAME")
   private String nane;
 
-  @Column(name = "TEAM_ID")
-  private Long teamId;
-  //멤버와 팀을 레퍼런스로 가져가야 하는데 지금은 그냥 DB에 맞춰서 모델링을 한 것.
+//  @Column(name = "TEAM_ID")
+//  private Long teamId;
+//  //멤버와 팀을 레퍼런스로 가져가야 하는데 지금은 그냥 DB에 맞춰서 모델링을 한 것.
+
+  @ManyToOne
+  @JoinColumn(name = "TEAM_ID")
+  private Team team;
+  //Member 입장에서는 여러 멤버가 하나의 팀에 소속될 수 있으므로.
+  // 그리고 객체에 있는 Team team;레퍼런스랑. Member 테이블의 TEAM_ID(FK)랑 매핑을 해야.
 
 
   public Long getId() {
@@ -34,7 +42,7 @@ public class Member {
     this.id = id;
   }
 
-  public String usernane() {
+  public String getNane() {
     return nane;
   }
 
@@ -42,11 +50,11 @@ public class Member {
     this.nane = nane;
   }
 
-  public Long getTeamId() {
-    return teamId;
+  public Team getTeam() {
+    return team;
   }
 
-  public void setTeamId(Long teamId) {
-    this.teamId = teamId;
+  public void setTeam(Team team) {
+    this.team = team;
   }
 }
