f9083931a286d11aa5aad6bdbf707acb5a52ca44
EUIJOON <euijoonism@gmail.com>
2023-08-17 15:00:51 +0900

    섹션 5. 연관관계 매핑 기초 - 양방향 연관관계와 연관관계 주인 (2) 주의점 (1)

--- src/main/java/inflearn/exjpa/jpaExample/JpaMain.java
+++ src/main/java/inflearn/exjpa/jpaExample/JpaMain.java
@@ -16,29 +16,28 @@ public class JpaMain {
     tx.begin();
 
     try {
-      //팀 저장
+      //저장
+//      Member member = new Member();
+//      member.setUsername("member1");
+//      em.persist(member);
+//
+//      Team team = new Team();
+//      team.setName("TeamA");
+//      team.getMembers().add(member);
+//      em.persist(team);
+
+      // 순서도 조금 바꿔서
       Team team = new Team();
       team.setName("TeamA");
       em.persist(team);
 
-      //회원 저장
       Member member = new Member();
       member.setUsername("member1");
-//      member.setTeamId(team.getId()); // member.setTeam(); 이 아니다.
       member.setTeam(team);
-      // 이러면 Jpa가 팀에서 알아서 pk값을 꺼내서 Insert할 때 FK 값으로 사용한다.
-
       em.persist(member);
 
       em.flush();
       em.clear();
-
-      Member findMember = em.find(Member.class, member.getId());
-      List<Member> members = findMember.getTeam().getMembers();
-
-      for (Member m1 : members) {
-        System.out.println("m = " + member.getUsername());
-      }
 
       tx.commit();
     } catch (Exception e) {
