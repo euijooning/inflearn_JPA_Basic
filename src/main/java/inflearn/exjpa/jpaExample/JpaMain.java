4a491252a2a84500e5d3420d0f264cc4a3f147f3
EUIJOON <euijoonism@gmail.com>
2023-08-21 22:51:32 +0900

    섹션 9. 값 타입 - 값 타입 컬렉션 대안

--- src/main/java/inflearn/exjpa/jpaExample/JpaMain.java
+++ src/main/java/inflearn/exjpa/jpaExample/JpaMain.java
@@ -1,7 +1,5 @@
 package inflearn.exjpa.jpaExample;
 
-import java.util.List;
-import java.util.Set;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -21,13 +19,15 @@ public class JpaMain {
       member.setUsername("member1");
       member.setHomeAddress(new Address("homeCity", "street", "10000")); //Member 테이블에 값들이 들어갈 거예요.
 
-      // 이 FAVORITEFOODS 컬렉션에 세 가지가 들어간다.
-      member.getFavoriteFoods().add("치킨");
-      member.getFavoriteFoods().add("족발");
-      member.getFavoriteFoods().add("피자");
+//      // 이 FAVORITEFOODS 컬렉션에 세 가지가 들어간다.
+//      member.getFavoriteFoods().add("치킨");
+//      member.getFavoriteFoods().add("족발");
+//      member.getFavoriteFoods().add("피자");
 
-      member.getAddressHistory().add(new Address("oldCity1", "street", "10000"));
-      member.getAddressHistory().add(new Address("oldCity2", "street", "10000"));
+//      member.getAddressHistory().add(new Address("oldCity1", "street", "10000"));
+//      member.getAddressHistory().add(new Address("oldCity2", "street", "10000"));
+      member.getAddressHistory().add(new AddressEntity("oldCity1", "street", "10000"));
+      member.getAddressHistory().add(new AddressEntity("oldCity2", "street", "10000"));
 
       em.persist(member); // 저장
 
@@ -51,12 +51,12 @@ public class JpaMain {
       findMember.getFavoriteFoods().remove("치킨");
       findMember.getFavoriteFoods().add("한식");
 
-      // 이제 주소 바꾸기 , 값 타입 인스턴스를 통으로 갈아끼워야한다는 것 명심.
-      // 컬렉션들은 값을 찾을 때 equals()로 찾느다.
-      findMember.getAddressHistory().remove(new Address("oldCity1", "street", "10000"));
-      // equals And hashcode 앞서 구현해놓았기 때문에 사용 가능
-      // 그리고 add도 해주기
-      findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
+//      // 이제 주소 바꾸기 , 값 타입 인스턴스를 통으로 갈아끼워야한다는 것 명심.
+//      // 컬렉션들은 값을 찾을 때 equals()로 찾느다.
+//      findMember.getAddressHistory().remove(new Address("oldCity1", "street", "10000"));
+//      // equals And hashcode 앞서 구현해놓았기 때문에 사용 가능
+//      // 그리고 add도 해주기
+//      findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
 
       tx.commit();
     } catch (Exception e) {
--- src/main/java/inflearn/exjpa/jpaExample/Member.java
+++ src/main/java/inflearn/exjpa/jpaExample/Member.java
@@ -1,11 +1,11 @@
 package inflearn.exjpa.jpaExample;
 
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import javax.persistence.AttributeOverride;
-import javax.persistence.AttributeOverrides;
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
@@ -14,6 +14,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -37,10 +38,16 @@ public class Member {
   @Column(name = "FOOD_NAME") // 얘는 값이 String 하나고 내가 정의한 게 아니므로. 예외적 허용.
   private Set<String> favoriteFoods = new HashSet<>();
 
-  @ElementCollection
-  @CollectionTable(name = "ADDRESS",
-                  joinColumns = @JoinColumn(name = "MEMBER_ID")) // 이러면 얘를 외래키로 잡게 됨.
-  private List<Address> addressHistory = new ArrayList<>();
+//  @ElementCollection
+//  @CollectionTable(name = "ADDRESS",
+//                  joinColumns = @JoinColumn(name = "MEMBER_ID")) // 이러면 얘를 외래키로 잡게 됨.
+//  private List<Address> addressHistory = new ArrayList<>();
+
+  //위에 것을 아래처럼 만들 수 있음.
+  // 일대다 단방향 매핑으로 만들자.
+  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+  @JoinColumn(name = "MEMBER_ID")
+  private List<AddressEntity> addressHistory = new ArrayList<>();
 
   public Long getId() {
     return id;
@@ -74,11 +81,19 @@ public class Member {
     this.favoriteFoods = favoriteFoods;
   }
 
-  public List<Address> getAddressHistory() {
+//  public List<Address> getAddressHistory() {
+//    return addressHistory;
+//  }
+//
+//  public void setAddressHistory(List<Address> addressHistory) {
+//    this.addressHistory = addressHistory;
+//  }
+
+  public List<AddressEntity> getAddressHistory() {
     return addressHistory;
   }
 
-  public void setAddressHistory(List<Address> addressHistory) {
+  public void setAddressHistory(List<AddressEntity> addressHistory) {
     this.addressHistory = addressHistory;
   }
 }
